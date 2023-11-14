da3fa8d04994e3907a230c9124bff3b9891644b5
Tanakamonzaemon <2201225@s.asojuku.ac.jp>
2023-11-14 10:50:25 +0900

    a

--- 開発/G1-1/script/script.js
+++ 開発/G1-1/script/script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const passwordInput = document.getElementById("exampleInputPassword1");
-    const showPasswordText = document.getElementById("showPassword");
+    const viewIcon = document.getElementById("view");
 
-    showPasswordText.addEventListener("click", function () {
+    viewIcon.addEventListener("click", function () {
         if (passwordInput.type === "password") {
             passwordInput.type = "text";
+            viewIcon.innerHTML = '<i class="far fa-eye"></i>';
         } else {
             passwordInput.type = "password";
+            viewIcon.innerHTML = '<i class="far fa-eye-slash"></i>';
         }
     });
 });
