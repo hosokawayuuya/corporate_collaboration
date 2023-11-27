b7c1215a4ea0d15b30dabb1082942803844304d2
hosokawayuuya <147389515+hosokawayuuya@users.noreply.github.com>
2023-11-27 16:58:27 +0900

    骨組み完成1.1.1

---
+++ 開発1.1.1/others/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ヘッダー</title>
+    <link rel="stylesheet" href="../Sample/template/css/vertical-layout-light/style.css">
+    <link rel="stylesheet" href="../Sample/template/vendors/typicons.font/font/typicons.css">
+    <link rel="stylesheet" href="../Sample/template/vendors/css/vendor.bundle.base.css">
+    <link rel="shortcut icon" href="../Sample/template/images/favicon.png" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
---
+++ 開発1.1.1/others/hooter.php
@@ -0,0 +1,15 @@
+<script src="../Sample/template/vendors/js/vendor.bundle.base.js"></script>
+<script src="js/off-canvas.js"></script>
+<script src="../Sample/template/js/off-canvas.js"></script>
+<script src="../Sample/template/js/hoverable-collapse.js"></script>
+<script src="../Sample/template/js/template.js"></script>
+<script src="../Sample/template/js/settings.js"></script>
+<script src="../Sample/template/js/todolist.js"></script>
+<script src="../Sample/template/vendors/progressbar.js/progressbar.min.js"></script>
+<script src="../Sample/template/vendors/chart.js/Chart.min.js"></script>
+<script src="../Sample/template/js/dashboard.js"></script>
+</body>
+</html>
+<?php
+ $pdo = null;   //DB切断
+ ?>
