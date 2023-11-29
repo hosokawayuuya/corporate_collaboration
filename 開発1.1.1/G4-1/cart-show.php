7e93c8163d7e05b7333b65503926f6660aca9562
hosokawayuuya <147389515+hosokawayuuya@users.noreply.github.com>
2023-11-29 23:53:39 +0900

    hennkou

--- 開発1.1.1/G4-1/cart-show.php
+++ 開発1.1.1/G4-1/cart-show.php
@@ -1,5 +1,7 @@
 <?php session_start();?>
 <?php require '../others/head.php';?>
 <?php require '../others/header.php';?>
+<br>
+<br>
 <?php require 'cart.php';?>
 <?php require '../others/footer.php'; ?>
