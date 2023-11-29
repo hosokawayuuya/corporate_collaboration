f5b2f0cf32149e588450bf725dbe1d73878f3619
hosokawayuuya <147389515+hosokawayuuya@users.noreply.github.com>
2023-11-29 20:48:53 +0900

    カートあと少し

--- 開発1.1.1/G3-2/Shohin.php
+++ 開発1.1.1/G3-2/Shohin.php
@@ -14,12 +14,16 @@ foreach($sql as $row){
     echo '<p><img alt="image" height="400" src="../image/',$row['gazou_id'],'"></p>';
     echo '<form action="../G4-1/cart-insert.php" method="post">';
     echo '<p>商品番号：',$row['shohin_id'],'</p>';
+    echo '<p>商品名：',$row['shohin_name'],'</p>';
+    echo '<p>価格：',$row['price'],'</p>';
     echo '<p>個数：<select name="count">';
     for($i=1;$i<=10;$i++){
         echo '<option value="',$i,'">',$i,'</option>';
     }
     echo '</select></p>';
     echo '<input type="hidden" name="shohin_id" value="',$row['shohin_id'],'">';
+    echo '<input type="hidden" name="shohin_name" value="',$row['shohin_name'],'">';
+    echo '<input type="hidden" name="price" value="',$row['price'],'">';
     echo '<p><input type="submit" value="カートに追加"></p>';
     echo '</form>';
     echo '<p><a href="favorite-insert.php?shohin_id=',$row['shohin_id'],
