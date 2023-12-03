90ddf2f4c2ac76961aba01718410af65c840081e
Tanakamonzaemon <2201225@s.asojuku.ac.jp>
2023-12-03 23:26:24 +0900

    a
    a

--- 開発1.1.1/G1-1/login-input.php
+++ 開発1.1.1/G1-1/login-input.php
@@ -1,29 +1,48 @@
 <?php
-require '../others/db-connect.php';
+$host = 'mysql217.phy.lolipop.lan';
+$dbname = 'LAA1517470-inful';
+$user = 'LAA1517470';
+$pass = 'Influenza';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    // その他の設定などを行うことができます
+} catch (PDOException $e) {
+    exit("データベースに接続できませんでした。エラー: " . $e->getMessage());
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // フォームからのデータを取得
     $loginId = $_POST['user_id'];
     $password = $_POST['password'];
 
+    // フォームの入力が空でないことを確認
     if (empty($loginId) || empty($password)) {
         $error = "ログインIDまたはパスワードを入力してください！";
     } else {
         // ログインIDを基にユーザーを取得するクエリ
         $query = "SELECT * FROM User WHERE user_id = :loginId";
-        // 適切なデータベース接続でクエリを実行
-
+        
+        // プリペアドステートメントを使用してクエリを実行
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':loginId', $loginId, PDO::PARAM_STR);
+        $stmt->execute();
+        
         // ユーザーデータを取得
-        // $user = ... // クエリの結果からユーザーデータを取得
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($user && isset($user['user_id'])) {
+            // パスワードが一致するか確認
             if (password_verify($password, $user['password'])) {
-                // パスワードが正しい場合
-                header("Location: ../G2-1/index.php");
+                // ログイン成功
+                header("Location:../G2-1/index.php");
                 exit();
             } else {
+                // パスワードが一致しない場合の処理
                 $error = "パスワードが一致しません";
             }
         } else {
+            // ユーザーが存在しない場合または'user_id'キーが存在しない場合の処理
             $error = "ユーザーの登録がありません！";
         }
     }
@@ -45,23 +64,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
     <h2>ログイン</h2>
     <form action="login-input.php" method="post">
-        <label for="loginId">ログインID</label>
-        <input type="text" name="user_id" required><br>
+        <label for="user_id">ログインID</label>
+        <input type="text" name="user_id" ><br>
 
         <label for="password">パスワード</label>
-        <input type="password" name="password" required><br>
+        <input type="password" name="password" ><br>
 
         <input type="submit" value="Login">
     </form>
 
     <?php
-    if (isset($error)) {
+    if(isset($error)) {
         echo "<p class='error'>$error</p>";
     }
     ?>
 </body>
 </html>
-
-
-
-
