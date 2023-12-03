81d896a0ebd759bc0c54bd47a9d06a96c4438c72
Tanakamonzaemon <2201225@s.asojuku.ac.jp>
2023-12-04 01:23:43 +0900

    hohhoa
    ajoafpoahha

--- 開発1.1.1/G1-1/login-input.php
+++ 開発1.1.1/G1-1/login-input.php
@@ -6,7 +6,7 @@ $pass = 'Influenza';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    // その他の設定などを行うことができます
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     exit("データベースに接続できませんでした。エラー: " . $e->getMessage());
 }
@@ -18,26 +18,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // フォームの入力が空でないことを確認
     if (empty($loginId) || empty($password)) {
-        $error = "ログインIDまたはパスワードを入力してください！";
+      $error = "ログインIDまたはパスワードが入力されていません。<br>登録していない方は下の新規登録からアカウントを作成してください。";
+
     } else {
         // ログインIDを基にユーザーを取得するクエリ
         $query = "SELECT * FROM User WHERE user_id = :loginId";
-        
+
         // プリペアドステートメントを使用してクエリを実行
         $stmt = $pdo->prepare($query);
         $stmt->bindParam(':loginId', $loginId, PDO::PARAM_STR);
         $stmt->execute();
-        
+
         // ユーザーデータを取得
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
+       
 
         if ($user && isset($user['user_id'])) {
             // パスワードが一致するか確認
             if (password_verify($password, $user['password'])) {
                 // ログイン成功
-                header("Location:../G2-1/index.php");
+                header("Location: ../G2-1/index.php");
                 exit();
             } else {
+                // パス
                 // パスワードが一致しない場合の処理
                 $error = "パスワードが一致しません";
             }
@@ -53,8 +56,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン</title>
+    <link rel="stylesheet" href="../Sample/template/vendors/typicons.font/font/typicons.css">
+  <link rel="stylesheet" href="../Sample/template/vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="../Sample/template/css/vertical-layout-light/style.css">
+  <link rel="shortcut icon" href="../Sample/template/images/favicon.png" />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .error {
             color: red;
@@ -62,21 +77,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </style>
 </head>
 <body>
-    <h2>ログイン</h2>
+<div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+              <div class="brand-logo" style="text-align: center">
+                <img src="..\image\AsoCityロゴ2.png" alt="ここに画像アイコン入る">
+              </div>
+   
     <form action="login-input.php" method="post">
-        <label for="user_id">ログインID</label>
-        <input type="text" name="user_id" ><br>
+    <label for="user_id">ログインID</label><br>
+    <input type="text" name="user_id" class="form-control form-control-lg"><br>
 
-        <label for="password">パスワード</label>
-        <input type="password" name="password" ><br>
+    <label for="password">パスワード</label><br>
+<input type="password" name="password" id="txtPass" class="form-control form-control-lg"><br>
+<span id="buttonEye" class="fa fa-eye-slash" onclick="togglePasswordVisibility()"><br></span>
+<script>
+function togglePasswordVisibility() {
+    var txtPass = document.getElementById("txtPass");
+    var btnEye = document.getElementById("buttonEye");
 
-        <input type="submit" value="Login">
-    </form>
-
-    <?php
+    if (txtPass.type === "password") {
+        txtPass.type = "text";
+        btnEye.className = "fa fa-eye";
+    } else {
+        txtPass.type = "password";
+        btnEye.className = "fa fa-eye-slash";
+    }
+}
+</script>
+<?php
     if(isset($error)) {
         echo "<p class='error'>$error</p>";
     }
     ?>
+    <input type="submit" value="ログイン" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn">
+</form>
+<div class="text-center mt-4 font-weight-light">
+  <br>
+                <a href="../G1-2/customer.php" class="text-primary">ーーーー新規登録ーーーー</a>
+                <div class="text-center mt-4 font-weight-light">
+                  <a href="../G2-1/index.php" class="text-primary">ーーログインせずに進むーー</a>
+                </div><br>
+
+   
 </body>
 </html>
