b1f0251b95193fb67eca1c0f9d274cdb2e78b305
hosokawayuuya <147389515+hosokawayuuya@users.noreply.github.com>
2023-12-04 19:53:11 +0900

    変更

--- 開発1.1.1/G2-1/index.php
+++ 開発1.1.1/G2-1/index.php
@@ -20,33 +20,34 @@ if(isset($_POST['keyword'])){
 }else{
     $sql=$pdo->query('select * from Shohin');
 }
-$products = $sql->fetchAll(PDO::FETCH_ASSOC);               
+$products = $sql->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <div class="container">
     <h1 class="my-4">商品一覧</h1>
     <div class="row">
-    <?php foreach($products as $row){
-    $id=$row['shohin_id']; ?>
-        <div class="col-md-4 mb-4">
-            <div class="card">
-                <h1 class="card-text"><?php echo $row['shohin_catch'] ?></h1>
-                <div class="example">
-                    <a href="../G3-2/Shohin.php?shohin_id= <?php echo $row['shohin_id'] ?>">
-                        <img src="../image/<?php echo $row['gazou_id'] ?>" class="card-img-top" alt="商品の画像">
-                    </a>
-                    <button id="hart" class="hart">&#10084;</button>
-                </div>
-                <div class="card-body">
-                    <h5 class="category"><i>#<?php echo $row['cate1'] ?> #<?php echo $row['cate2'] ?>#<?php echo $row['cate3'] ?></i></h5>
-                    <h5 class="card-title"><?php echo $row['shohin_name'] ?></h5>
-                    <p class="card-text"><?php echo $row['shohin_setu'] ?></p>
-                    <p class="card-text font-weight-bold">評価</p>
-                    <p class="card-text font-weight-bold"><?php echo $row['price'] ?>円</p>
+
+        <?php foreach($products as $row){
+        $id=$row['shohin_id']; ?>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <h1 class="card-text"><?php echo $row['shohin_catch'] ?></h1>
+                    <div class="example">
+                        <a href="../G3-2/Shohin.php?shohin_id= <?php echo $row['shohin_id'] ?>">
+                            <img src="../image/<?php echo $row['gazou_id'] ?>" class="card-img-top" alt="商品の画像">
+                        </a>
+                        <button id="hart" class="hart">&#10084;</button>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="category"><i>#<?php echo $row['cate1'] ?> #<?php echo $row['cate2'] ?>#<?php echo $row['cate3'] ?></i></h5>
+                        <h5 class="card-title"><?php echo $row['shohin_name'] ?></h5>
+                        <p class="card-text"><?php echo $row['shohin_setu'] ?></p>
+                        <p class="card-text font-weight-bold">評価</p>
+                        <p class="card-text font-weight-bold"><?php echo $row['price'] ?>円</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    }
+        <?php }?>
     </div>
 </div>
 <?php require '../others/footer.php'; ?>
