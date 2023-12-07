9b3c3b75582dff65525d074836c9a80fcbb20f3f
sota20040308 <2201124@s.asojuku.ac.jp>
2023-12-07 12:34:27 +0900

    dd

--- 開発1.1.1/G6-1/mypage.php
+++ 開発1.1.1/G6-1/mypage.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title></title>
-    <link rel="stylesheet" href="../Sample/template/vendors/typicons.font/font/typicons.css">
-    <link rel="stylesheet" href="../Sample/template/vendors/css/vendor.bundle.base.css">
-    <link rel="stylesheet" href="../Sample/template/css/vertical-layout-light/style.css">
-    <link rel="shortcut icon" href="../Sample/template/images/favicon.png" />
-    <link rel="stylesheet" href="css/style.css">
-    <style>
-      .example {
-        text-align: center;
-        padding: 10px;
-        margin-top: 10px;
-      }
-
-      .custom-btn {
-        width: 400px; /* ボタンの幅を調整する値 */
-        height: 70px; /* ボタンの高さを調整する値 */
-      }
-    </style>
-  </head>
-  <body>
-    <!-- ここから書き換えてください -->
+<?php require '../others/head.php'; ?>
+<?php require '../others/header.php'; ?>
     <div class="example">
       <a href="../G6-3/mypage.php">
         <button type="button" class="btn btn-light btn-rounded btn-fw custom-btn">設定変更</button>
@@ -56,6 +32,16 @@
     <div class="example">
     <button type="button" class="btn btn-light btn-rounded btn-fw custom-btn"  onclick="history.back()">戻る</button>
 </div>
-    <!-- ここまで書き換えてください -->
-  </body>
-</html>
+<style>
+      .example {
+        text-align: center;
+        padding: 10px;
+        margin-top: 10px;
+      }
+
+      .custom-btn {
+        width: 400px; /* ボタンの幅を調整する値 */
+        height: 70px; /* ボタンの高さを調整する値 */
+      }
+ </style>
+ <?php require '../others/footer.php'; ?>
