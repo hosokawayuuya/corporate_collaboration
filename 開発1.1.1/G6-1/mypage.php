866e5dcc343d50ec75f6779fc55f645057d995af
hosokawayuuya <147389515+hosokawayuuya@users.noreply.github.com>
2023-12-07 12:34:55 +0900

    こみつ

--- 開発1.1.1/G6-1/mypage.php
+++ 開発1.1.1/G6-1/mypage.php
@@ -26,7 +26,7 @@
     <!-- ここから書き換えてください -->
     <div class="example">
       <a href="../G6-3/mypage.php">
-        <button type="button" class="btn btn-secondary btn-rounded btn-fw">設定変更</button>
+        <button type="button" class="btn btn-light btn-rounded btn-fw custom-btn">設定変更</button>
       </a>
     </div>
 
