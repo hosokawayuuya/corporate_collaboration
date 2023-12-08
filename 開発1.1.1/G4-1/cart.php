2126fb5ca2d0b30e5ed394a1073bc71ffeedfefd
hosokawayuuya <147389515+hosokawayuuya@users.noreply.github.com>
2023-12-08 11:55:30 +0900

    komitu

--- 開発1.1.1/G4-1/cart.php
+++ 開発1.1.1/G4-1/cart.php
@@ -34,7 +34,7 @@
                 <br>
                 <p class="card-text1 font-weight-bold">合計:￥<?php echo $total ?>円</p><br>
                 <div class="define">
-                <button type="button" onclick="location.href='../G5-1/purchace.php'"  class="btn btn-primary">購入に進む</button>
+                <button type="button" onclick="location.href='../G5-1/purchace-input.php'"  class="btn btn-primary">購入に進む</button>
                 </div>
             </div>
         <?php
