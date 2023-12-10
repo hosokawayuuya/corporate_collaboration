5478ec6cbdfd34fed6587fef75e714b3d827f04c
fujirai <2201188@s.asojuku.ac.jp>
2023-12-11 00:18:09 +0900

    aaa
    a

--- 開発1.1.1/G2-1/index.php
+++ 開発1.1.1/G2-1/index.php
@@ -94,11 +94,11 @@ $products = $sql->fetchAll(PDO::FETCH_ASSOC);
                         
                     </div>
                     <div class="card-body">
-                        <h4 class="category"><i>#<?php echo $row['cate1'] ?> #<?php echo $row['cate2'] ?>#<?php echo $row['cate3'] ?></i></h5>
-                        <h3 class="card-title"><?php echo $row['shohin_name'] ?></h5>
+                        <h4 class="category"><i>#<?php echo $row['cate1'] ?> #<?php echo $row['cate2'] ?>#<?php echo $row['cate3'] ?></i></h4>
+                        <h3 class="card-title"><?php echo $row['shohin_name'] ?></h3>
                         <p class="card-text"><?php echo $row['shohin_setu'] ?></p>
                         <p class="card-text font-weight-bold">評価</p>
-                        <p class="card-text font-weight-bold"><?php echo $row['price'] ?>円</p>
+                        <h3 class="card-text font-weight-bold"><?php echo $row['price'] ?>円</h3>
                     </div>
                 </div>
             </div>
